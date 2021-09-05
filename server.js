ee85a8fb8675b38f60577392089e124b98de583d
nqbang1999 <54302546+nqbang1999@users.noreply.github.com>
2021-09-05 09:36:14 +0700

    09:36

--- server.js
+++ server.js
@@ -286,21 +286,19 @@ route.post('/bangquang11/delete', deleteChallenge);
 const axios = require('axios')
 
 homeRoute = (req, res) => {
-    try {
         // make a req api
         axios.get('http://localhost:3000/bangquang11/updateByCurrentTime')
             .then(function () {
                 console.log('qua updateByCurrentTime chua')
             }).catch(err => {
-            throw err}
-    })
+            throw error})
 
     axios.get('http://localhost:3000/bangquang11/all')
         .then(function (response) {
             res.render('home', {challenges: response.data});
         })
         .catch(err => {
-            throw error
+            throw err
         })
 }
 route.get('/', homeRoute);
